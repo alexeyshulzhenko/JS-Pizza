acb5a9f14e1cd87ba5bc5f2347ce495ec7beef49
Alexey Shulzhenko <alexey shulzhenko>
2016-05-31 09:02:48 +0300

    Uhhhhh

--- Frontend/src/pizza/PizzaCart.js
+++ Frontend/src/pizza/PizzaCart.js
@@ -1,38 +1,59 @@
 /**
  * Created by chaika on 02.02.16.
  */
-var Templates = require('../Templates');
 
-//Перелік розмірів піци
+var Templates = require('../Templates');
+var Storage=require('../Storage');
+
+
+//Sizes
 var PizzaSize = {
     Big: "big_size",
     Small: "small_size"
 };
 
-//Змінна в якій зберігаються перелік піц в кошику
 var Cart = [];
+var total=0;
 
-//HTML едемент куди будуть додаватися піци
-var $cart = $("#cart");
+//HTML element where pizzas situated
+var $cart = $("#card");
 
 function addToCart(pizza, size) {
     //Додавання однієї піци в кошик покупок
-    
+
     //Приклад реалізації, можна робити будь-яким іншим способом
-    Cart.push({
-        pizza: pizza,
-        size: size,
-        quantity: 1
-    });
+    var add_new=true;
+    for( var a=0;a<Cart.length;a++){
+        if((pizza===Cart[a].pizza)&&(size===Cart[a].size)) {
+            Cart[a].quantity += 1;
+            add_new=false;
+        }
+    }
+    if(add_new===true){
+        Cart.push({
+            pizza: pizza,
+            size: size,
+            quantity: 1
+        });
+    }
 
     //Оновити вміст кошика на сторінці
+    total += pizza[size].price;
+    $("#totality").text(total+" грн.");
     updateCart();
 }
 
+
+//Після видалення оновити відображення
 function removeFromCart(cart_item) {
     //Видалити піцу з кошика
-    //TODO: треба зробити
+    var html_code = Templates.PizzaCart_OneItem(cart_item);
 
+    var $node = $(html_code);
+    $node.find(".count-clear").click(function(){
+        $node.remove();
+    });
+    Cart.splice(Cart.indexOf(cart_item), 1);
     //Після видалення оновити відображення
     updateCart();
 }
@@ -40,15 +61,24 @@ function removeFromCart(cart_item) {
 function initialiseCart() {
     //Фукнція віпрацьвуватиме при завантаженні сторінки
     //Тут можна наприклад, зчитати вміст корзини який збережено в Local Storage то показати його
-    //TODO: ...
+    var saved_pizza=Storage.get('cart');
+    if(saved_pizza){
+        Cart=saved_pizza;
+    }
+
+    var saved_sum=Storage.get('totality');
+    if(saved_sum){
+        $("#totality").text(saved_sum+" грн.");
+        total=parseInt($("#totality").text());
+    }
 
     updateCart();
 }
 
 function getPizzaInCart() {
-    //Повертає піци які зберігаються в кошику
     return Cart;
 }
+
 
 function updateCart() {
     //Функція викликається при зміні вмісту кошика
@@ -62,20 +92,37 @@ function updateCart() {
         var html_code = Templates.PizzaCart_OneItem(cart_item);
 
         var $node = $(html_code);
-
-        $node.find(".plus").click(function(){
+        var pricing=parseInt($node.find(".price").text());
+        
+        $node.find(".plusBtn").click(function(){
             //Збільшуємо кількість замовлених піц
             cart_item.quantity += 1;
 
             //Оновлюємо відображення
             updateCart();
         });
+        
+        $node.find(".minusBtn").click(function(){
+            //Збільшуємо кількість замовлених піц
+            cart_item.quantity -= 1;
 
+            //Оновлюємо відображення
+            updateCart();
+        });
         $cart.append($node);
+        
+        $node.find(".count-clear").click(function () {
+            removeFromCart(cart_item);
+          //  total -= pricing*counter;
+          //  $("#totality").text(total+" грн.");
+            updateCart();
+        });
+        
     }
-
+    
     Cart.forEach(showOnePizzaInCart);
 
+    Storage.set("cart",Cart);
 }
 
 exports.removeFromCart = removeFromCart;
@@ -85,3 +132,6 @@ exports.getPizzaInCart = getPizzaInCart;
 exports.initialiseCart = initialiseCart;
 
 exports.PizzaSize = PizzaSize;
+
+
+
--- Frontend/src/pizza/PizzaMenu.js
+++ Frontend/src/pizza/PizzaMenu.js
@@ -1,6 +1,3 @@
-/**
- * Created by chaika on 02.02.16.
- */
 var Templates = require('../Templates');
 var PizzaCart = require('./PizzaCart');
 var Pizza_List = require('../Pizza_List');
@@ -8,43 +5,123 @@ var Pizza_List = require('../Pizza_List');
 //HTML едемент куди будуть додаватися піци
 var $pizza_list = $("#pizza_list");
 
-function showPizzaList(list) {
-    //Очищаємо старі піци в кошику
-    $pizza_list.html("");
 
     //Онволення однієї піци
-    function showOnePizza(pizza) {
-        var html_code = Templates.PizzaMenu_OneItem({pizza: pizza});
+    function showPizzaList(list) {
+        //Очищаємо старі піци в кошику
+        $pizza_list.html("");
 
-        var $node = $(html_code);
+        //Онволення однієї піци
+        function showOnePizza(pizza) {
+            var html_code = Templates.PizzaMenu_OneItem({pizza: pizza});
 
-        $node.find(".buy-big").click(function(){
-            PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Big);
-        });
-        $node.find(".buy-small").click(function(){
-            PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Small);
-        });
+            var $node = $(html_code);
+            //var need_to_pay=0;
 
-        $pizza_list.append($node);
+            $node.find(".buy-big").click(function(){
+                console.log("buy big");
+                PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Big);
+            });
+            $node.find(".buy-small").click(function(){
+                console.log("buy small");
+                PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Small);
+            });
+
+            $pizza_list.append($node);
+        }
+
+        list.forEach(showOnePizza);
     }
-
-    list.forEach(showOnePizza);
-}
 
 function filterPizza(filter) {
     //Масив куди потраплять піци які треба показати
     var pizza_shown = [];
+    var total_in_type=0;
+            if (filter === "all")pizza_shown = Pizza_List;
+            else if (filter === "vega") {
+                Pizza_List.forEach(function (pizza) {
+                    if (pizza.type === 'Вега') {
+                        pizza_shown.push(pizza);
+                        total_in_type++;
+                    }
+                });
+            }
+            else if (filter === "meat") {
+                Pizza_List.forEach(function (pizza) {
+                    if (pizza.type === 'М’ясна піца'){
+                        pizza_shown.push(pizza);
+                        total_in_type++;
+                    }
+                });
+            }
+            else if (filter === "pineapple") {
+                Pizza_List.forEach(function (pizza) {
+                    if (pizza.content.pineapple){
+                        pizza_shown.push(pizza);
+                        total_in_type++;
+                    }
+                });
+            }
 
-    Pizza_List.forEach(function(pizza){
-        //Якщо піка відповідає фільтру
-        //pizza_shown.push(pizza);
+            else if (filter === "mushroom") {
+                Pizza_List.forEach(function (pizza) {
+                    if (pizza.content.mushroom){
+                        pizza_shown.push(pizza);
+                        total_in_type++;
+                    }
+                });
+            }
 
-        //TODO: зробити фільтри
-    });
-
-    //Показати відфільтровані піци
+            else if (filter === "ocean") {
+                Pizza_List.forEach(function (pizza) {
+                    if (pizza.content.ocean){
+                        pizza_shown.push(pizza);
+                        total_in_type++;
+                    }
+                });
+            }
     showPizzaList(pizza_shown);
+
 }
+
+
+$("#all").click(function(){
+    $(".allPizzas").text("Усі піцци:"+8);
+  //  $("#IK").text(5);
+    filterPizza('all');
+});
+
+$("#meat").click(function(){
+   $(".allPizzas").text("М'ясні піци:"+5);
+    filterPizza("meat");
+});
+
+
+$("#pineapple").click(function(){
+    $(".allPizzas").text("Піци з ананасами:"+3);
+    filterPizza('pineapple');
+});
+
+
+$("#mushroom").click(function(){
+    $(".allPizzas").text("Піци з грибами:"+3);
+    filterPizza('mushroom');
+});
+
+
+$("#ocean").click(function(){
+    $(".allPizzas").text("Піци з морепродуктами:"+2);
+    filterPizza('ocean');
+});
+
+
+$("#vega").click(function(){
+    $(".allPizzas").text("Вегетаріанські піци:"+1);
+    filterPizza('vega');
+});
+
+
+
 
 function initialiseMenu() {
     //Показуємо усі піци
